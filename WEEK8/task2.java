2a45cfb1b55f3db6cc7cc2d879bb11210233a392
Navyasree Rokkam <vtu27489@veltech.edu.in>
2026-09-12 21:34:28 +0530

    Add MotorCycle class with inheritance from BiCycle
    Implemented MotorCycle class extending BiCycle and fixed method call.

---
+++ WEEK8/task2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class BiCycle {
+    String define_me() {
+        return "a vehicle with pedals.";
+    }
+}
+
+class MotorCycle extends BiCycle {
+    @Override
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+    
+    MotorCycle() {
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+        
+        // Fix this line using the super keyword to call the parent class method
+        String temp = super.define_me(); 
+        
+        System.out.println("My ancestor is a cycle who is " + temp);
+    }
+}
+
+class Solution {
+    public static void main(String []args) {
+        MotorCycle M = new MotorCycle();
+    }
+}
+
+
+output
+  Expected Output
+Hello I am a motorcycle, I am a cycle with an engine.
+My ancestor is a cycle who is a vehicle with pedals.
